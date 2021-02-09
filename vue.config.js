8d237c48fcf0cc041615d38dacb4d14be8abfe75
zyw <1158998912@qq.com>
2021-02-09 11:56:39 +0800

    初始化

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+module.exports = {
+    chainWebpack: config => {
+        const svgRule = config.module.rule('svg')
+        svgRule.uses.clear()
+        svgRule
+            .test(/\.svg$/)
+            .include.add(path.resolve(__dirname, './src/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+        const fileRule = config.module.rule('file')
+        fileRule.uses.clear()
+        fileRule
+            .test(/\.svg$/)
+            .exclude.add(path.resolve(__dirname, './src/icons'))
+            .end()
+            .use('file-loader')
+            .loader('file-loader')  }}
